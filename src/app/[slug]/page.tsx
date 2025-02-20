f8441b6254d51663f03a3ef9e827478640f41fd8
erkzz1 <erickmathias.contato@gmail.com>
2025-02-19 23:14:52 -0300

    :sparkles: feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consuption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="gap2 flex flex-col items-center">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* BEM VINDO */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+        <div className="grid grid-cols-2 gap-4 pt-14">
+          <ConsumptionMethodOption
+            option="DINE_IN"
+            slug= {restaurant.slug}
+            buttonText="Para comer aqui!"
+            imageAlt="Comer aqui"
+            imageUrl="/dine_in.png"
+          />
+          <ConsumptionMethodOption
+            option="TAKEAWAY"
+            slug= {restaurant.slug}
+            buttonText="Para levar!"
+            imageAlt="Para levar"
+            imageUrl="/takeaway.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
