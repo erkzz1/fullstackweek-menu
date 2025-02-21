1a8e95402859e5d5405cb84ee6413b38512cb32b
erkzz1 <erickmathias.contato@gmail.com>
2025-02-21 02:24:41 -0300

    :sparkles: feat: improve addProduct function to handle duplicate products in cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -31,7 +31,23 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
+    const productIsAlreadyOntheCart = products.some(
+      (prevProduct) => prevProduct.id == product.id,
+    );
+    if (!productIsAlreadyOntheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id == product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
   };
   return (
     <CartContext.Provider
