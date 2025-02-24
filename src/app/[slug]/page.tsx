db9e665f09edde438ecdf875c2ce218ab61b1cda
erkzz1 <erickmathias.contato@gmail.com>
2025-02-24 04:19:29 -0300

    :art: refactor: Correct import of the consumptionMethodoration component and adjust formatting in the restaurant page code

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
-import ConsumptionMethodOption from "./components/consuption-method-option";
+import ConsumptionMethodOption from "../[slug]/components/consuption-method-option";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -20,7 +20,7 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   return (
     <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
       {/* LOGO E TITULO */}
-      <div className="gap2 flex flex-col items-center">
+      <div className="flex flex-col items-center gap-2">
         <Image
           src={restaurant.avatarImageUrl}
           alt={restaurant.name}
@@ -39,14 +39,14 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
         <div className="grid grid-cols-2 gap-4 pt-14">
           <ConsumptionMethodOption
             option="DINE_IN"
-            slug= {restaurant.slug}
+            slug={restaurant.slug}
             buttonText="Para comer aqui!"
             imageAlt="Comer aqui"
             imageUrl="/dine_in.png"
           />
           <ConsumptionMethodOption
             option="TAKEAWAY"
-            slug= {restaurant.slug}
+            slug={restaurant.slug}
             buttonText="Para levar!"
             imageAlt="Para levar"
             imageUrl="/takeaway.png"
