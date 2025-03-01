7d7cb4f3c55a8f5e6979e988672765abedb1774d
erkzz1 <erickmathias.contato@gmail.com>
2025-02-28 21:55:46 -0300

    :sparkles: feat: add stripe and webhook integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 const HomePage = () => {
-  return (
-    <div className="justify-items-center">
-      <h1>Bem vindo! Selecione o restaurante!</h1>
-      <Link href="/fsw-donalds">
-        <Button><p>Ir para o restaurante Donalds</p></Button>
-      </Link>
-    </div>
-  );
-};
+  redirect ("/fsw-donalds");
+
+  return null
+}
 
 export default HomePage;
